d90460997c519993478b1111771ecf491250ae48
umayer10-AI <mdumayer10@gmail.com>
2026-05-25 15:35:03 +0600

    add

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 "use strict";
+// class A {
+//     constructor(public name: string, public age: number){}
 Object.defineProperty(exports, "__esModule", { value: true });
+//         getName() {
+//             return this.name;
+//         }
+//         setName(v:string){
+//             this.name = v
+//         }
+// }
+// const b = new A("Umayer", 21)
+// // b.getName()
+// // b.name="oka oka"
+// b.setName("Lala")
+// // console.log(b.getName())
+// console.log(b)
 class A {
-    name;
+    _name;
     age;
-    constructor(name, age) {
-        this.name = name;
+    constructor(_name, age) {
+        this._name = _name;
         this.age = age;
     }
-    getName() {
-        return this.name;
+    get getName() {
+        return this._name;
     }
-    setName(v) {
-        this.name = v;
+    set setName(v) {
+        this._name = v;
     }
 }
 const b = new A("Umayer", 21);
-// b.getName()
-b.name = "oka oka";
-// b.setName("Lala")
-// console.log(b.getName())
+b.setName = "hola";
 console.log(b);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,18 +1,34 @@
-class A {
-    constructor(public name: string, public age: number){}
+// class A {
+//     constructor(public name: string, public age: number){}
 
-        getName() {
-            return this.name;
-        }
+//         getName() {
+//             return this.name;
+//         }
 
-        setName(v:string){
-            this.name = v
-        }
+//         setName(v:string){
+//             this.name = v
+//         }
 
-}
-const b = new A("Umayer", 21)
-// b.getName()
-b.name="oka oka"
+// }
+// const b = new A("Umayer", 21)
+// // b.getName()
+// // b.name="oka oka"
 // b.setName("Lala")
-// console.log(b.getName())
+// // console.log(b.getName())
+// console.log(b)
+
+
+class A {
+    constructor(public _name: string , public age?: number){}
+
+    get getName(){
+        return this._name
+    }
+
+    set setName(v: string) {
+        this._name = v
+    }
+}
+const b = new A("Umayer",21)
+b.setName = "hola"
 console.log(b)
